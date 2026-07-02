9af6c05ce812540e4d84a58e760c34b6d1faad8f
abdurraufafridi071-beep <abdurraufafridi071@gmail.com>
2026-07-02 07:52:55 +0500

    Add image preview and search functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+const image = document.getElementById("image");
+const preview = document.getElementById("preview");
+
+if(image){
+    image.addEventListener("change", function(){
+        const file = this.files[0];
+
+        if(file){
+            preview.src = URL.createObjectURL(file);
+            preview.style.display = "block";
+        }
+    });
+}
+const searchInput = document.getElementById("searchInput");
+
+if (searchInput) {
+  searchInput.addEventListener("input", function () {
+    const value = this.value.toLowerCase();
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+      const text = card.textContent.toLowerCase();
+
+      if (text.includes(value)) {
+        card.style.display = "";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+}
